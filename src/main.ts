86ffd2ebf0ca7cbae684299a31d3440175e47985
Jhonny <jhonnycgarcia@gmail.com>
2025-06-04 23:58:53 -0500

    Configurar variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
     })
   );
     
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.PORT);
+
+  console.log(`Server is running on port ${envs.PORT}`);
 }
 bootstrap();
