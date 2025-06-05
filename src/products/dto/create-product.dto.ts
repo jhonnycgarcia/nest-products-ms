1c9f73ec4a9e0f2094dbd120ac9860647f27c9f5
Jhonny <jhonnycgarcia@gmail.com>
2025-06-04 23:43:16 -0500

    Entidad y DTOs

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,13 @@
-export class CreateProductDto {}
+import { Type } from "class-transformer";
+import { IsString, MinLength, IsNumber, Min } from "class-validator";
+
+export class CreateProductDto {
+    @IsString()
+    @MinLength(1)
+    name: string;
+
+    @IsNumber({maxDecimalPlaces: 4})
+    @Min(0)
+    @Type(() => Number)
+    price: number;
+}
