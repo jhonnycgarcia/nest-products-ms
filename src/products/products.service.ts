e40e95ae4fb7bcefe1f6ca299a248dab20b09da3
Jhonny <jhonnycgarcia@gmail.com>
2025-06-05 17:42:14 -0500

    Eliminar un producto

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -62,7 +62,11 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    // hard delete
+    await this.findOne(id);
+    return this.product.delete({
+      where: { id },
+    });
   }
 }
